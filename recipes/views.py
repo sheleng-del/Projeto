9efc793a90d888fc649ad84a5d219028adf9844d
sheleng-del <shelengoedert@gmail.com>
2023-09-22 16:44:57 -0300

    html

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={
-        'name': 'Shelen Gonsalves'
-    })
+    return render(request, 'recipes/pages/home.html',)
